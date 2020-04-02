5eda6043c1014d51e666ad9e3d4000b49de9f0f4
kashif shahzad <r.kashifshahzad@outlook.com>
2020-04-02 14:19:48 +0500

    front basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Web3 from 'web3'
+import NavBar from './components/NavBar'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3(){
+    if (window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.web3
+    //load accounts
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({ account: accounts[0] })
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <NavBar account = {this.state.account}/>
+        <div></div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
